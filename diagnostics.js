706cf920b19b11b015c2486f89141a456a87dd2b
Alejandro Madriñán Fernández <36339608+alexmadrinanfdez@users.noreply.github.com>
2026-09-15 13:33:31 +0200

    cleaner unit model

--- diagnostics.js
+++ diagnostics.js
@@ -6,6 +6,17 @@ function getAspectRatio(width, height) {
   return `${width / divisor}:${height / divisor}`;
 }
 
+function formatDimensions(width, height) {
+  return [width, String.fromCodePoint(0x00D7), height, "px"].join(" ");
+}
+
+function formatNumber(value, compact = false) {
+  const divisor = compact ? 1_000_000 : 1;
+  const unit = compact ? "Mpx" : "px";
+
+  return `${(value / divisor).toLocaleString()} ${unit}`;
+}
+
 function getDiagnostics() {
   const screenWidth = screen.width;
   const screenHeight = screen.height;
@@ -27,25 +38,27 @@ function getDiagnostics() {
 function renderDiagnostics(diagnostics) {
   document.getElementById("width").textContent = diagnostics.screenWidth;
   document.getElementById("height").textContent = diagnostics.screenHeight;
-  document.getElementById("resolution").textContent = `${diagnostics.totalPixels}px`;
-  document.getElementById("screen-size").textContent = `${diagnostics.screenWidth} x ${diagnostics.screenHeight}`;
-  document.getElementById("available-size").textContent = `${diagnostics.availableWidth} x ${diagnostics.availableHeight}`;
-  document.getElementById("viewport-size").textContent = `${diagnostics.viewportWidth} x ${diagnostics.viewportHeight}`;
+  document.getElementById("resolution").textContent = formatNumber(diagnostics.totalPixels, compact=true)
+    + ` = ${formatNumber(diagnostics.totalPixels * diagnostics.devicePixelRatio ** 2, compact=true)} physical (device)`;
+  document.getElementById("screen-size").textContent = formatDimensions(diagnostics.screenWidth, diagnostics.screenHeight);
+  document.getElementById("available-size").textContent = formatDimensions(diagnostics.availableWidth, diagnostics.availableHeight);
+  document.getElementById("viewport-size").textContent = formatDimensions(diagnostics.viewportWidth, diagnostics.viewportHeight);
   document.getElementById("device-pixel-ratio").textContent = diagnostics.devicePixelRatio;
   document.getElementById("aspect-ratio").textContent = diagnostics.aspectRatio;
   document.getElementById("orientation").textContent = diagnostics.orientation;
-  document.getElementById("total-pixels").textContent = diagnostics.totalPixels.toLocaleString();
+  document.getElementById("total-pixels").textContent = formatNumber(diagnostics.totalPixels);
+  document.getElementById("total-pixels").title = `= ${formatNumber(diagnostics.totalPixels, compact=true)}`;
 }
 
 function formatDiagnostics(diagnostics) {
   return [
-    `Screen: ${diagnostics.screenWidth} x ${diagnostics.screenHeight} CSS px`,
-    `Available screen: ${diagnostics.availableWidth} x ${diagnostics.availableHeight} CSS px`,
-    `Browser viewport: ${diagnostics.viewportWidth} x ${diagnostics.viewportHeight} CSS px`,
+    `Screen: ${formatDimensions(diagnostics.screenWidth, diagnostics.screenHeight)}`,
+    `Available screen: ${formatDimensions(diagnostics.availableWidth, diagnostics.availableHeight)}`,
+    `Browser viewport: ${formatDimensions(diagnostics.viewportWidth, diagnostics.viewportHeight)}`,
     `Device pixel ratio: ${diagnostics.devicePixelRatio}`,
     `Aspect ratio: ${diagnostics.aspectRatio}`,
     `Orientation: ${diagnostics.orientation}`,
-    `Total screen pixels: ${diagnostics.totalPixels.toLocaleString()}`
+    `Total screen pixels: ${formatNumber(diagnostics.totalPixels)} (${formatNumber(diagnostics.totalPixels, compact=true)})`
   ].join("\n");
 }
 
